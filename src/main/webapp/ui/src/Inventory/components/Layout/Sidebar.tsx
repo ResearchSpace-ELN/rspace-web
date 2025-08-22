8943e449ecbe1ed0536fd776a28d623528aeae3f
Robert Lamacraft <robert.lamacraft@researchspace.com>
2025-08-22 14:21:45 +0100

    Divider after create menu

--- src/main/webapp/ui/src/Inventory/components/Layout/Sidebar.tsx
+++ src/main/webapp/ui/src/Inventory/components/Layout/Sidebar.tsx
@@ -508,6 +508,7 @@ function Sidebar({ id }: SidebarArgs): React.ReactNode {
     <CustomDrawer id={id}>
       <Box ref={sidebarRef} aria-label="Inventory Sidebar Navigation">
         <CreateNew onClick={afterClick} />
+        <Divider />
         <Box
           {...eventHandlers}
           sx={{
