a0023a23dc8ce9abcd2299b563ea73559e8771ed
Nico Ferrante <nicola.ferrante@researchspace.com>
2026-01-22 09:33:57 +0000

    RSDEV-XXX: Fix hibernate exception when calling getGroupInfo

--- src/main/java/com/researchspace/api/v1/controller/GroupApiController.java
+++ src/main/java/com/researchspace/api/v1/controller/GroupApiController.java
@@ -12,7 +12,7 @@ import com.researchspace.model.dtos.GroupSearchCriteria;
 import com.researchspace.service.GroupManager;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Set;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import javax.ws.rs.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -64,14 +64,8 @@ public class GroupApiController extends BaseApiController implements GroupApi {
   @Override
   public ApiGroupInfo getUserGroupById(
       @PathVariable("id") Long id, @RequestAttribute(name = "user") User user) {
-    Set<Group> groups = groupManager.listGroupsForUser();
-    return groups.stream()
-        .filter(g -> g.getId().equals(id))
-        .findFirst()
-        .map(ApiGroupInfo::new)
-        .orElseThrow(
-            () ->
-                new NotFoundException(
-                    "Group with id: " + id + " not found, or the user isn't a member."));
+    Optional<ApiGroupInfo> groupInfoOptional = groupManager.getGroupInfoById(id);
+    return groupInfoOptional.orElseThrow(() -> new NotFoundException(
+        "Group with id: " + id + " not found, or the user isn't a member."));
   }
 }
