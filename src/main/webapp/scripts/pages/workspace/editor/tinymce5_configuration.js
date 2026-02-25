fcb375a2c7dade9aab4650087be147ae2ee21005
Timothy Choi <timothy.choi@researchspace.com>
2026-02-25 10:20:34 +0100

    Move tinymce directory to match version

--- src/main/webapp/scripts/pages/workspace/editor/tinymce5_configuration.js
+++ src/main/webapp/scripts/pages/workspace/editor/tinymce5_configuration.js
@@ -88,7 +88,7 @@ var tinymcesetup = {
 	lineheight_formats: '9px 10px 12px 14px 16px 20px 24px 30px 40px 64px',
 	gecko_spellcheck: true,
 	relative_urls: false,
-	content_css: ["/styles/simplicity/typoEdit.css", "/scripts/tinymce/tinymce516/plugins/codesample/css/prism.css"],
+	content_css: ["/styles/simplicity/typoEdit.css", "/scripts/tinymce/tinymce5109/plugins/codesample/css/prism.css"],
 	image_advtab: true,
 	smart_paste: false,
 	branding: false,
